52ca052de3f778035cf8590948f9808a51746595
Joe Wigmar <joewigmar@gmail.com>
2025-10-31 14:44:51 -0300

    feat: setup basic quiz questions and initial App component

--- quiz-app-react/src/App.jsx
+++ quiz-app-react/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+const questions = [
+  {
+    questionText: 'What is the capital of France?',
+    answerOptions: [
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'Dublin', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who is CEO of Tesla?',
+    answerOptions: [
+      { answerText: 'Jeff Bezos', isCorrect: false },
+      { answerText: 'Elon Musk', isCorrect: true },
+      { answerText: 'Bill Gates', isCorrect: false },
+      { answerText: 'Tony Stark', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the capital of India?',
+    answerOptions: [
+      { answerText: 'Dhaka', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Paris', isCorrect: false },
+      { answerText: 'Dehli', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'What is the capital of Afghanistan?',
+    answerOptions: [
+      { answerText: 'Dehli', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Kabul', isCorrect: true },
+      { answerText: 'Dublin', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the capital of Pakistan?',
+    answerOptions: [
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'Islamabad', isCorrect: true },
+      { answerText: 'Paris', isCorrect: false },
+      { answerText: 'Peshawar', isCorrect: false },
+    ],
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Quiz App</div>
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
